97cc086f90778713850b014d1994e6f251a602e5
Ecosystem123 <abhinandps031@gmail.com>
2026-08-31 09:04:16 +0530

    started after long break

---
+++ modules/metrics_collector/metrics_collector.py
@@ -0,0 +1,23 @@
+import requests
+from prometheus_client.parser import text_string_to_metric_families
+try:
+  s = requests.get('http://localhost:9835/metrics')
+  print("local system gpu details are: \n")
+  data = {}
+  k = s.text
+  for i in  text_string_to_metric_families(k):
+   for j in i.samples:
+     if j.name == 'nvidia_smi_memory_used_bytes':
+       print("nvidia_smi_memory_used_bytes:",j.value)
+       data["nvidia_smi_memory_used_bytes"] = j.value
+     if j.name == 'nvidia_smi_memory_total_bytes':
+       print("nvidia_smi_memory_total_bytes:",j.value)
+       data["nvidia_smi_memory_total_bytes"] = j.value
+     if j.name == 'nvidia_smi_temperature_gpu':
+       print("nvidia_smi_temperature_gpu:",j.value)
+       data["nvidia_smi_temperature_gpu"] = j.value
+     if j.name == 'nvidia_smi_power_draw_average_watts':
+       print("nvidia_smi_power_draw_average_watts:",j.value)
+       data["nvidia_smi_power_draw_average_watts"] = j.value
+except Exception as e:
+ print(e)
